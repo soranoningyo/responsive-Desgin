6d9269bc299288df212c9e39d74087999e58d601
sora <sora0no0ningyo@gmail.com>
2019-03-30 20:45:38 +0200

    price section was added (and linked with header)

--- js/scriptsheet.js
+++ js/scriptsheet.js
@@ -20,3 +20,31 @@ $(window).resize(function () {
         paddingTop: ($(window).height() - $('.hc').height()) / 2
     });
 });
+ // Toggling Active Mode
+
+$('.price .plan').mouseenter(function () {
+    'use strict';
+
+    $(this).addClass('active').siblings().removeClass('active');
+});
+$('.price .plan').mouseleave(function () {
+    'use strict';
+
+    $('.price .plan:nth-of-type(2)').addClass('active').siblings().removeClass('active');
+});
+
+ // smooth scroll
+
+$('header .hc .thplans').click(function () {
+
+    'use strict';
+
+    $('html ,body').animate({
+
+        scrollTop: $('#plans').offset().top
+
+    }, 1000);
+
+    $('#' + $(this).data('value')).addClass('active').siblings().removeClass('active');
+
+});
